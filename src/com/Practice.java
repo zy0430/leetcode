8b9a09c11f14d251873a1d9e97fc6cfac07c32d5
LAPTOP-UDSNSP26\章鱼哥 <648883413@qq.com>
2019-11-26 22:56:32 +0800

    add T14

---
+++ src/com/Practice.java
@@ -0,0 +1,10 @@
+package com;
+
+public class Practice {
+    public static void main(String[] args) {
+        String a = "111abc";
+        String b = "ab";
+
+        System.out.println(a.indexOf(b));
+    }
+}
